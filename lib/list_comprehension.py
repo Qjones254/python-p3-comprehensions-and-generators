512a8c11408c0e09e19cfb34b258664e834e9f61
quincy mwangi <mwangiquincy77@gmail.com>
2024-09-12 09:53:16 +0300

    complete list comprehension lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return[num for num in num_list if num % 2 == 0]
+numbers = [1,2,3,4,5,6,7,8]
+even_numbers = return_evens(numbers)
+print(even_numbers)
+    
 
-def make_exclamation(sentence_list):
-    pass
+def make_exclamation(sentence_strings):
+    return[string + "!" for string in sentence_strings]
+string = "Hello" "I'm doing great" "Python is fun"
+new_string = make_exclamation(string)
+print(new_string)
